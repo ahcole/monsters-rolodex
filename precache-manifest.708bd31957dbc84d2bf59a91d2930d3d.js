69a36f37681b1f9141caa36fd49d5c4e88b8e2f2
ahcole <al.hawkinscole@gmail.com>
2020-05-29 17:05:47 +0100

    Updates

---
+++ precache-manifest.708bd31957dbc84d2bf59a91d2930d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343e36808efb6a82c990747cbd67c3ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "756118fa34efe8bbc19d",
+    "url": "/monsters-rolodex/static/css/main.6c863b31.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756118fa34efe8bbc19d",
+    "url": "/monsters-rolodex/static/js/main.348916e1.chunk.js"
+  },
+  {
+    "revision": "aa1aeb4ed7e62e71a38e",
+    "url": "/monsters-rolodex/static/js/runtime-main.2292ee2f.js"
+  }
+]);
